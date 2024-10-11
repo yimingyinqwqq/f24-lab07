be4f99e173679ec22f92d9f71b45406f47370b49
edison <yimingy9@illinois.edu>
2024-10-10 23:47:56 -0400

    fix: finish fake testing

--- src/main/java/AndrewWebServices/InMemoryDatabase.java
+++ src/main/java/AndrewWebServices/InMemoryDatabase.java
@@ -4,9 +4,16 @@ package AndrewWebServices;
  * InMemoryDatabase is a fake for the AndrewWS database which is used to improve test efficiency.
  * Remember, fakes are fully functional classes with simplified implementation.
  * What is the simplest core functionality that we need for a functional database?
- * 
+ *
  * Hint: there are two methods you need to implement
  */
-public class InMemoryDatabase /* should there be something here? */ {
+public class InMemoryDatabase extends Database {
     // Implement your fake database here
+    public int getPassword(String accountName) {
+        if (accountName.equals("Scotty")) {
+            return 17214;
+        } else {
+            return 0;
+        }
+    }
 }
